b7a416926d0c412d008c9c063f0330220a6f7b9a
Toni Tassani <atassani@gmail.com>
2019-04-20 16:34:02 +0200

    Fixed print list

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # pygot
+
+
+export EDITOR=vi
+crontab -e
+
+*/5 * * * * /usr/bin/python /home/osmc/pygot/scrap.py
+
+sudo journalctl -r
--- scrap.py
+++ scrap.py
@@ -5,6 +5,9 @@ from bs4 import BeautifulSoup
 import os
 import re
 
+episode = 's08e01'
+tvShow = 'game of thrones'
+fileLock = '/home/osmc/downloading_got'
 
 def simple_get(url):
     """
@@ -18,11 +21,9 @@ def simple_get(url):
                 return resp.content
             else:
                 return None
-
     except RequestException as e:
         log_error('Error during requests to {0} : {1}'.format(url, str(e)))
         return None
-
 
 def is_good_response(resp):
     """
@@ -32,7 +33,6 @@ def is_good_response(resp):
     return (resp.status_code == 200
             and content_type is not None
             and content_type.find('html') > -1)
-
 
 def log_error(e):
     """
@@ -47,8 +47,7 @@ class Torrent:
         units = {"B": 1, "KB": 10**3, "MB": 10**6, "GB": 10**9, "TB": 10**12}
         number, unit = [string.strip() for string in size.split()]
         return int(float(number)*units[unit])
-
-    def __init__(self, title, size, magnet, link, age, seeds):
+    def __init__(self, title, size, magnet, link, age, seeds, sortKey):
         self.title = title
         self.size = size
         self.magnet = magnet
@@ -56,26 +55,30 @@ class Torrent:
         self.age = age
         self.seeds = seeds
         self.size_in_bytes = self.__parseSize(size)
+        self.sortKey = sortKey
     def __str__(self):
         return self.title + ' '+ self.size + ' ' + str(self.seeds)
     def __eq__(self, other):
-#        return self.seeds == other.seeds
-        return self.size_in_bytes == other.size_in_bytes
+        if self.sortKey == 'seeds':
+            return self.seeds == other.seeds
+        else:
+            return self.size_in_bytes == other.size_in_bytes
     def __lt__(self, other):
-#        return self.seeds < other.seeds
-        return self.size_in_bytes < other.size_in_bytes
+        if self.sortKey == 'seeds':
+            return self.seeds < other.seeds
+        else:
+            return self.size_in_bytes < other.size_in_bytes
 
 # Check if already downloading
-exists = os.path.isfile('/home/osmc/downloading_got')
+exists = os.path.isfile(fileLock)
 if exists:
     quit()
 
-episode = 's08e02'
-
-raw_html = simple_get('https://eztv.io/search/game-of-thrones-' + episode)
+eztvSearchString = (tvShow + ' ' + episode).replace(' ', '_').lower()
+raw_html = simple_get('https://eztv.io/search/' + eztvSearchString)
 html = BeautifulSoup(raw_html, 'html.parser')
 torrents = []
-expr = re.compile('game of thrones ' + episode, re.I)
+expr = re.compile(tvShow + ' ' + episode, re.I)
 for p in html.select('tr.forum_header_border'):
     row = p.select('td')
     title = row[1].text.strip()
@@ -86,16 +89,19 @@ for p in html.select('tr.forum_header_border'):
             row[2].find('a', 'magnet').get('href'),
             row[2].find('a', 'download_1').get('href'),
             row[4].text,
-            int(row[5].text.replace(',', '')))
+            int(row[5].text.replace(',', '')),
+            'seeds'
+            )
         torrents.append(t)
 
 if len(torrents) == 0:
     quit()
 
-sortedTorrents = sorted(torrents, reverse=True) # REVERSE TRUE
-#print(*sortedTorrents, sep='\n')
+sortedTorrents = sorted(torrents, reverse=True)
+
+#print('\n'.join(map(str, sortedTorrents)))
 
 # Create empty file to avoid relaunch
-open('/home/osmc/downloading_got', 'a').close()
+open(fileLock, 'a').close()
 
 os.system('transmission-remote --add ' + sortedTorrents[0].magnet)
